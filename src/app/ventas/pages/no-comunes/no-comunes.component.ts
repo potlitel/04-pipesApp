7c9dbc879cbce570fa9323fe5f227e5fd7f1f544
potlitel <potlitel@gmail.com>
2023-02-10 12:00:04 -0500

    Se completa el trabajo con el async pipe.

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -10,6 +11,8 @@ export class NoComunesComponent {
   //i18nSelect
   nombre: string = 'Fernando';
   genero: string = 'masculino';
+
+  miNumero: number = 0;
 
   invitacionMapa = {
     'masculino':'invitarlo',
@@ -38,5 +41,40 @@ export class NoComunesComponent {
     nombre: 'Fernando',
     edad: 35,
     direccion: 'Ottawa, Cánada'
+  }
+
+  //Json pipe
+  heroes = [
+    {
+      nombre:'Superman',
+      vuela: true
+    },
+    {
+      nombre:'Robin',
+      vuela: false
+    },
+    {
+      nombre:'Aquaman',
+      vuela: false
+    }
+  ]
+
+  //Async Pipe
+  miObservable = interval(5000).pipe(
+    tap( () => {
+      console.log('interval');
+    } )
+  );
+
+  valorPromesa = new Promise( (resolve, reject) =>{
+    setTimeout(() => {
+      resolve('Tenemos datos de promesa');
+    }, 3500);
+  } );
+
+  constructor(){
+    this.miObservable.subscribe(valor => {
+        this.miNumero = valor
+    });
   }
 }
